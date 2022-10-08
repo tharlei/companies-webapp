fed0768188689247b9b0c25ce6d39991d0cc2f3b
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-10-08 15:40:53 -0300

    feat: add colors of palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          200: '#f3f6f7',
+          400: '#e9eced',
+          500: '#9ea8ac',
+          800: '#7b7e7e',
+        },
+        indigo: {
+          400: '#608289',
+          800: '#203B43',
+        },
+        green: '#28cc56',
+      },
+    },
   },
   plugins: [],
 };
