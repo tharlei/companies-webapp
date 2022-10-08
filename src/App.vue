bb241351beb7fb5924cf8286be51e6e91b1ce755
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-10-08 10:51:21 -0300

    feat: add tailwindcss lib

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello world</h1>
+  <h1 class="text-red-800">Hello world</h1>
 </template>
 
-<style scoped></style>
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
