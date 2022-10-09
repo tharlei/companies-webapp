4bcb3edfa80ecf075d3ad9029bbc24b8d885f122
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-10-09 12:15:05 -0300

    feat: add services in page

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,18 +1,82 @@
 <script setup lang="ts">
-import { onUpdated, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
+import { City, getCities } from '../../services/get-cities';
+import { FederalUnit, getFederalUnits } from '../../services/get-federal-units';
+import { Company, getCompanies } from '../../services/get-companies';
+import { debounce } from '../../shared/debounce';
+import NotFound from '../../assets/not-found.svg';
 
 const name = ref('');
+const page = ref(1);
 const federalUnit = ref('');
 const city = ref('');
 const loading = ref(true);
+const federalUnits = ref<FederalUnit[]>([]);
+const cities = ref<City[]>([]);
+const companies = ref<Company[]>([]);
+const canLoadMore = ref(false);
 
 const headerTitle = 'Empresas ' + new Date().getFullYear();
+const limit = 15;
 
-onUpdated(() => {
+watch(federalUnit, async () => {
+  const promises = [handleCompanies()];
+
   if (!federalUnit.value) {
     city.value = '';
   }
+
+  if (federalUnit.value) {
+    promises.push(handleCities(federalUnit.value));
+  }
+
+  Promise.all(promises);
 });
+
+watch(city, async () => {
+  await handleCompanies();
+});
+
+watch(name, debounce(handleCompanies, 500));
+
+onMounted(async () => {
+  const [federalUnitsCollection] = await Promise.all([
+    getFederalUnits(),
+    handleCompanies(),
+  ]);
+
+  federalUnits.value = federalUnitsCollection;
+});
+
+async function handleCompanies() {
+  loading.value = true;
+  const { collection, existsNextPage } = await getCompanies({
+    page: 1,
+    limit: limit * page.value,
+    federal_unit: federalUnit.value,
+    city: city.value,
+    name: name.value,
+  });
+
+  companies.value = collection;
+  canLoadMore.value = existsNextPage;
+  loading.value = false;
+}
+
+async function handleCities(federalUnit: string) {
+  cities.value = await getCities(federalUnit);
+}
+
+async function loadingMoreCompanies() {
+  page.value++;
+  await handleCompanies();
+}
+
+function resetFilters() {
+  federalUnit.value = '';
+  city.value = '';
+  name.value = '';
+}
 </script>
 
 <template>
@@ -33,7 +97,7 @@ onUpdated(() => {
               v-model="name"
               type="text"
               placeholder="Digite nome"
-              class="bg-gray-200 rounded text-lg pb-2 pt-10 px-3 placeholder-gray-500 text-gray-800 border-b-2 focus:border-b-4 border-indigo-400 transition-all duration-75"
+              class="form-control-custom"
             />
             <div class="flex justify-end -mt-12 mr-6">
               <fa
@@ -47,12 +111,15 @@ onUpdated(() => {
             <label class="text-gray-500 text-base -mb-10 ml-3 z-10 font-medium">
               Estado
             </label>
-            <select
-              v-model="federalUnit"
-              class="cursor-pointer bg-gray-200 rounded text-lg pb-2 pt-11 px-2 placeholder-gray-500 text-indigo-400 border-b-2 focus:border-b-4 border-indigo-400 transition-all duration-75"
-            >
+            <select v-model="federalUnit" class="form-control-custom">
               <option value="">Todos</option>
-              <option value="SP">São Paulo</option>
+              <option
+                v-for="item in federalUnits"
+                :key="item.initials"
+                :value="item.initials"
+              >
+                {{ item.name }}
+              </option>
             </select>
           </div>
 
@@ -63,42 +130,65 @@ onUpdated(() => {
             <select
               v-model="city"
               :disabled="!federalUnit"
-              class="cursor-pointer bg-gray-200 rounded text-lg pb-2 pt-11 px-2 placeholder-gray-500 text-indigo-400 border-b-2 focus:border-b-4 border-indigo-400 transition-all duration-75"
+              class="form-control-custom"
             >
               <option value="">Todos</option>
-              <option value="Araçatuba">Araçatuba</option>
+              <option v-for="item in cities" :key="item.id" :value="item.name">
+                {{ item.name }}
+              </option>
             </select>
           </div>
         </div>
 
-        <div
-          class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mb-8 gap-8 text-indigo-400 font-semibold"
-        >
-          <div class="border border-gray-400 rounded p-4">
-            <h2 class="text-2xl">Empresa A</h2>
+        <div v-if="!loading && companies.length" class="list-companies">
+          <div
+            v-for="company in companies"
+            :key="company.id"
+            class="border border-gray-400 rounded p-4"
+          >
+            <h2 class="text-2xl">{{ company.name }}</h2>
             <p class="mt-2 text-base text-gray-800 font-normal">
-              Dusi diuqh wquidh wqhd hqwdhqwuh udhqwhduqwhduqwud
+              {{ company.description }}
             </p>
             <div class="flex items-center text-xl mt-8">
-              <a href="mailto:" class="mr-8">Enviar email</a>
-              <a href="https://wa.me/">Whatsapp</a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                :href="`mailto:${company.email}`"
+                class="mr-8"
+                >Enviar email</a
+              >
+              <a
+                target="_blank"
+                rel="noreferrer"
+                :href="`https://wa.me/${company.phone}`"
+                >Whatsapp</a
+              >
             </div>
           </div>
+        </div>
+        <div v-else-if="loading" class="list-companies"></div>
+        <div v-else class="flex flex-col justify-center items-center">
+          <img
+            :src="NotFound"
+            alt="Bixinhos mostrando interrogação, representando que não encontrou nada"
+          />
+          <p class="text-2xl text-center text-gray-800 font-medium my-8">
+            Nenhuma empresa encontrada :(
+          </p>
+          <button class="btn-custom" @click="resetFilters">
+            Limpe os filtros
+          </button>
         </div>
 
         <div class="flex justify-center">
           <button
-            class="bg-green py-4 px-14 text-xl rounded-lg text-white"
+            v-if="canLoadMore && !loading"
+            class="btn-custom"
             :disabled="loading"
+            @click="loadingMoreCompanies"
           >
-            <span v-if="loading">
-              <fa
-                :icon="['fas', 'circle-notch']"
-                class="font-bold text-xl animate-spin mr-2"
-              />
-              Carregando...
-            </span>
-            <span v-else>Carregar mais</span>
+            Carregar mais
           </button>
         </div>
       </div>
@@ -106,4 +196,16 @@ onUpdated(() => {
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.list-companies {
+  @apply grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mb-8 gap-8 text-indigo-400 font-semibold;
+}
+
+.btn-custom {
+  @apply bg-green py-4 px-14 text-xl rounded-lg text-white font-bold;
+}
+
+.form-control-custom {
+  @apply bg-gray-200 rounded text-lg pb-2 pt-10 px-3 placeholder-gray-500 text-gray-800 border-b-2 focus:border-b-4 border-indigo-400 transition-all duration-75;
+}
+</style>
