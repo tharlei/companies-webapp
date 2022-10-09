dd8f4e50f8b07e14e910a4e5f7f278d4a3cedbac
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-10-09 11:44:04 -0300

    feat: add service to get federal units

---
+++ src/services/get-federal-units.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+interface ApiFederalUnit {
+  nome: string;
+  sigla: string;
+}
+
+export interface FederalUnit {
+  initials: string;
+  name: string;
+}
+
+export async function getFederalUnits(): Promise<FederalUnit[]> {
+  try {
+    const { data } = await axios.get<ApiFederalUnit[]>(
+      'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
+    );
+
+    return data.map((federalUnit) => ({
+      initials: federalUnit.sigla,
+      name: federalUnit.nome,
+    }));
+  } catch {
+    return [];
+  }
+}
